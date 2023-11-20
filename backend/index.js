52887544fa9e812a1574e6cc67404aa02b6db405
isankha007 <sankhadeep2007@gmail.com>
2023-11-20 21:42:57 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { PORT } from './config.js';
+
+
+
+const app = express();
+
+app.get("/",(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Welocme to MERN Stack')
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening to port: ${PORT}`)
+})
