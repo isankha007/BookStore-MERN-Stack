44754cd2e4796f0e48a2b6ea2ee85a4475bc24bd
isankha007 <sankhadeep2007@gmail.com>
2023-11-21 19:06:03 +0530

    mongo book model added

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,54 @@
 import express from 'express'
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/booksModel.js';
+
 
 
 
 const app = express();
+app.use(express.json());
 
 app.get("/",(request,response)=>{
     console.log(request)
     return response.status(234).send('Welocme to MERN Stack')
 })
 
-app.listen(PORT,()=>{
-    console.log(`App is listening to port: ${PORT}`)
+app.post('/books',async (request,response) => {
+try {
+    if(!request.body.title ||
+        !request.body.author||
+        !request.body.publishYear){
+            response.status(400).send(
+                {message: 'Send all required field: title, author ,publishYear'}
+             )  
+        }
+} catch (error) {
+ console.log(error.message)  
+ response.status(500).send(
+    {message: error.message}
+ ) 
+}
+const newBook = {
+    title: request.body.title,
+    author: request.body.author,
+    publishYear: request.body.publishYear
+ };
+
+ console.log(request.body.title)
+ const book = await Book.create(newBook); 
+
+ return response.status(201).send(book);
 })
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database')
+    app.listen(PORT,()=>{
+        console.log(`App is listening to port: ${PORT}`)
+    })
+}
+).catch(()=>{
+
+});
